5e178580e907582517ad5b1a6140eb27c89475f2
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-17 20:26:51 -0500

    first commit

---
+++ src/components/AddItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+    render () {
+        return (
+            <div>
+                <h2>Welcome to My App</h2>
+            </div>
+        )
+    }
+}
+
+export default AddItem;
